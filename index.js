f6237f2699953efebef9c13e74870458bfd5b99b
Tayyaba10 <159796040+Tayyaba10@users.noreply.github.com>
2024-09-12 09:49:24 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+//
+import { differenceInSeconds } from "date-fns";
+//function for count down second
+function* countdownTimer(second) {
+    while (second > 0) {
+        yield second;
+        second--;
+    }
+}
+let timerIterater = countdownTimer(20);
+function displayCountdown() {
+    let result = timerIterater.next();
+    if (!result.done) {
+        const now = new Date();
+        const countdownTimer = new Date(now.getTime() + (result.value * 1000));
+        const remainingSeconds = differenceInSeconds(countdownTimer, now);
+        console.log(`Remaining seconds : ${remainingSeconds}`);
+        setTimeout(displayCountdown, 1000);
+    }
+    else {
+        console.log("Countdown Completed!");
+    }
+}
+displayCountdown();
